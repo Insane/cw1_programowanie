ab52cacb073b7cb5a6ae1998282a0f6388b5774c
Paulina B. <paulina.bednar@gmail.com>
2014-10-22 15:25:22 +0200

    Cwiczenie1- zadania wykonane

--- zadanie3.py
+++ zadanie3.py
@@ -1,1 +1,23 @@
+# -*- coding: utf-8 -*-
 
+from math import sqrt
+
+def funkcja_kwadratowa(a,b,c,x):
+    return a*(x**2)+b*x+c
+def miejsca_zerowe(a,b,c):
+    delta=b**2-4*a*c
+    x1=(-b-sqrt(delta))/(2*a)
+    x2=(-b+sqrt(delta))/(2*a)
+    if delta>0:
+        return x1,x2
+    elif delta==0:
+        return -b/(2*a)
+    else:
+        return None
+a=2
+b=3
+c=1
+for i in xrange(-10,11):
+    print "Dla parametrów a=%s, b=%s, c=%s, oraz dla:" %(a,b,c)
+    print "x= %s, funkcja kwadratowa przyjmuje wartość: %s, a jej miejsca zerowe to: %s" %(i,funkcja_kwadratowa(a, b, c, i), miejsca_zerowe(a,b,c))
+  
--- zadanie4.py
+++ zadanie4.py
@@ -1,1 +1,9 @@
+def duplikaty(lista):
+    dl=len(lista)
+    wynik=[]
+    for i in xrange(0,dl):
+        if wynik.count(lista[i])==0:
+                wynik.append(lista[i])
+    print wynik    
 
+duplikaty(["kot","pies","chomik","kot","pies","pies","wydra","okon"])
